8c339785cdbbc6f28c536527fe7582665fcd3483
TK <timlaja.1997@gmail.com>
2022-06-28 01:42:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './under.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>
+        WEBSITE IS CURRENTLY UNDER CONSTRUCTION
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+       We'll be back shortly!
+        </h2>
       </header>
     </div>
   );
